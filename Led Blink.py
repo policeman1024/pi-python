258424ccc44acc3fb29e1708048adcba13ed7f51
Manish Timilsina <manishtimilsina@gmail.com>
2020-05-07 20:39:52 +0200

    COmment added
    comment is added on line 10

--- Led Blink.py
+++ Led Blink.py
@@ -1,10 +1,13 @@
 import RPi.GPIO as GPIO
 import time
 
+
+
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(17,GPIO.OUT)
-'''while (input() == False)'''
+'''while (input() == False)'''#This is a comment
 print ("LED ON")
 GPIO.output(17,GPIO.HIGH)
 time.sleep(1)
